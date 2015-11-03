978ec11c9fa14386214f5a8617d699a6610a7ab3
ken restivo <ken@restivo.org>
2015-11-03 00:45:27 -0800

    First skeleton

---
+++ migrateusers.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+
+
+function migrateusers_load() {
+// register hooks
+	
+	// get all channels
+       
+	// allow getting a channel by proxy 
+	
+
+}
+
+function migrateusers_unload() {
+	// unregister hooks
+
+}
