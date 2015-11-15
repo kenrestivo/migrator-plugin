66b3c4138f4feded3a363834c933979730bded9f
ken restivo <ken@restivo.org>
2015-11-14 21:14:57 -0800

    Bikeshedding

--- migrator.php
+++ migrator.php
@@ -68,12 +68,12 @@ function export_channel_hashes(&$a, $account_id) {
 
 	}
 
-	$c = q("select channel_hash, channel_id from channel where channel_account_id = %d",
+	$c = q("select `channel_hash`, `channel_id` from `channel` where `channel_account_id` = %d",
 	       intval($account_id));
 
-	if(! $c){
+	if(count($c) < 1){
 		header('HTTP/1.0 404 Not Found');
-		die('No such account_id '. $account_id);
+		die('No such account_id '. intval($account_id));
 		
 	}			
 
